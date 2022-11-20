e0d729a337f8aaaa4c84678f720bfbe14bda5f63
svyatoslavxq <svyatoslavxq@yandex.ru>
2022-11-20 22:11:31 +0300

    fix: move index.html and delete style.scss as unnecessary

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import 'bootstrap/js/dist/modal';
+import 'bootstrap/scss/bootstrap.scss';
 import app from './application.js';
 
 app();
--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/index.html',
+      template: './index.html',
     }),
     new MiniCssExtractPlugin({
       filename: '[name].css',
